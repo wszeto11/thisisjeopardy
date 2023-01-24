653458a347a44d57da5501bbf7271e4929f0728c
Willard Szeto <wszeto11@gmail.com>
2023-01-24 10:01:11 -0500

    Star Trek, Knicks, and Empire State questions added

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ const gameCategories = [
         answers: ['Chrysler Sebring', 'Dodge Neon'],
         correct: 'Chrysler Sebring',
         value: '400',
+        // ^ make dailyDoubleOne
 
         question: 'Where did Pam go to art school?',
         answers: ['Philadelphia', 'NYC'],
@@ -37,12 +38,32 @@ const gameCategories = [
     topic: 'Star Trek',
     questions: [
       {
-        question: 'Which starship does Captain Picard command?',
+        question: 'Which starship has Captain Picard commanded?',
         answers: ['Enterprise', 'Apollo'],
         correct: 'Enterprise',
-        value: '100'
+        value: '100',
 
-        // add 4 more questions
+        question: "What is the name of Spock's home planet?",
+        answers: ['Romulus', 'Vulcan'],
+        correct: 'Vulcan',
+        value: '200',
+
+        question:
+          'Michelle Yeoh plays which character in Star Trek: Discovery?',
+        answers: ['Sulu', 'Philippa Georgiou'],
+        correct: 'Philippa Georgiou',
+        value: '300',
+
+        question:
+          'The USS Discovery utilizes what experimental propulsion system?',
+        answers: ['Spore Drive', 'Proto Drive'],
+        correct: 'Spore Drive',
+        value: '400',
+
+        question: 'In what year was the United Federation of Planets founded?',
+        answers: ['2213', '2161'],
+        correct: '2161',
+        value: '500'
       }
     ]
   },
@@ -53,9 +74,27 @@ const gameCategories = [
         question: 'Who is the latest Knick to be an all-star?',
         answers: ['Julius Randle', 'Bob Dole'],
         correct: 'Julius Randle',
-        value: '100'
+        value: '100',
 
-        // add 4 more questions
+        question: 'Where do the Knicks play their home games?',
+        answers: ['Barclay Center', 'Madison Square Garden'],
+        correct: 'Madison Square Garden',
+        value: '200',
+
+        question: 'Who was the last Knick to win a scoring title?',
+        answers: ['Carmelo Anthony', 'Patrick Ewing'],
+        correct: 'Carmelo Anthony',
+        value: '300',
+
+        question: 'When was the last time the Knicks won the Finals?',
+        answers: ['1999', '1973'],
+        correct: '1973',
+        value: '400',
+
+        question: 'How many head coaches have the Knicks had since 2009?',
+        answers: ['8', '5'],
+        correct: '8',
+        value: '500,'
       }
     ]
   },
@@ -66,9 +105,28 @@ const gameCategories = [
         question: 'What is the name of the tallest building in New York state?',
         answers: ['Empire State Building', 'Madison Square Garden'],
         correct: 'Empire State Building',
-        value: '100'
+        value: '100',
 
-        // add 4 more questions
+        question: 'What is the capital of New York?',
+        answers: ['Manhattan', 'Albany'],
+        correct: 'Albany',
+        value: '200',
+
+        question:
+          "How many pounds of confetti is used every year during the New Year's Eve celebration?",
+        answers: ['3,300', '2,200'],
+        correct: '3,300',
+        value: '300',
+
+        question: "What is New York's state bird?",
+        answers: ['Eastern Goldfinch', 'Eastern Bluebird'],
+        correct: 'Eastern Bluebird',
+        value: '400',
+
+        question: 'How many counties make up New York state?',
+        answers: ['62', '45'],
+        correct: '62',
+        value: '500'
       }
     ]
   },
@@ -76,7 +134,7 @@ const gameCategories = [
     topic: 'Superheroes',
     questions: [
       {
-        question: "What is Spiderman's real name?",
+        question: "What is Spider Man's real name?",
         answers: ['Eddie Brock', 'Peter Parker'],
         correct: 'Peter Parker',
         value: '100'
